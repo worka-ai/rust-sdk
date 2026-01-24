033a0de4a77088100033fe28d98a57cefee5a7b7
zcourts <courtney.robinson@hypi.io>
2026-01-24 15:07:31 +0000

    Use literalStringArray for A2UI string arrays

--- crates/rmcp/src/a2ui.rs
+++ crates/rmcp/src/a2ui.rs
@@ -66,7 +66,7 @@ impl BoolRef {
 pub struct StringArrayRef {
     #[serde(skip_serializing_if = "Option::is_none")]
     pub path: Option<String>,
-    #[serde(rename = "literalArray", skip_serializing_if = "Option::is_none")]
+    #[serde(rename = "literalStringArray", skip_serializing_if = "Option::is_none")]
     pub literal_array: Option<Vec<String>>,
 }
 
